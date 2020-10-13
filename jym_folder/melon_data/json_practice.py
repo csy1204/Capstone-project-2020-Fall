4d5fdcbdeb7809954659ce358ac2d139a861fe70
조영민 <joqjoq966@naver.com>
2020-10-13 17:19:10 +0900

    pull request practice

--- jym_folder/melon_data/json_practice.py
+++ jym_folder/melon_data/json_practice.py
@@ -6,7 +6,6 @@ file_data = OrderedDict()
 file_data["name"] = "Jo Youngmin"
 file_data["age"] = 26
 file_data["major"] = "Mathematics/Computer Engineering"
-file_data["address"] = " 시흥시 "
 
 print(json.dumps(file_data, ensure_ascii=False, indent="\t"))
 
